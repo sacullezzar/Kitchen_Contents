f10e3b57285ed235867c03f857610cdf1e1a7380
sacullezzar <lucas.razzell@gmail.com>
2018-09-24 19:37:47 +0100

    menu structure start

---
+++ directory.rb
@@ -0,0 +1,51 @@
+
+@freezer = [
+  {bread: 1.5},
+  {fish: 3.0},
+  {chips: 0.8},
+]
+
+def view_contents
+  loop do
+    menu
+    if @loc == "freezer"
+      puts "The freezer contains:"
+      @freezer.each do |item|
+        item.each_pair {|k, v| puts "- #{k} (x #{v})"}
+      end
+    elsif @loc == "exit"
+      exit
+    end
+  end
+end
+
+def menu
+  puts "Where to look?"
+  @loc = gets.chomp
+end
+
+def main
+  puts "Kitchen Stock Management"
+  puts "------============------"
+  puts "-------Main  Menu-------"
+end
+def selector
+  puts "Make your choice"
+  @selection = gets.chomp
+end
+
+def contents
+  puts "1. View Location"
+  puts "2. Add Ingredients"
+  puts "------------------"
+end
+
+
+main
+contents
+selector
+if @selection == "1"
+  view_contents
+else
+  exit
+end
